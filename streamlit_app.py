641515b983b024064b8a0a6604e63f5d5f3aba22
Nilesh Rawool <nileshdataworks@gmail.com>
2025-08-12 14:35:35 +0530

    Update streamlit_app.py
    Updated max selection to 6 to get 5 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
    'Choose up to 5 ingredients:'
    ,my_dataframe
-   ,max_selections=5 
+   ,max_selections=6 
 )
 
 if ingredients_list:
